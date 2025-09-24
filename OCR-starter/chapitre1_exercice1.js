03ca85930b5187a8959fbb930ef6cb9f28848ab5
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-24 12:18:04 +0200

    end ex 1.1 myFirst Creation of constant + concaner 2 constante & theorie

--- OCR-starter/chapitre1_exercice1.js
+++ OCR-starter/chapitre1_exercice1.js
@@ -4,10 +4,26 @@ Affichez dans la console :
 - votre âge (ce doit être un nombre)
 */
 
-const prenom = "julien"
-const age = 25
+//mots clef var c'est un mot clef reserver du langage que js a imposer on peux pas imposser le mot var a une variables
+//mettres en min la premiers lettres
+//pas de caracteres speciaux
+//nom anglais
+//le nom de la variables dois etres evocateur de la valeurs de la variables
+//var name = 'julien' ne jamais utiliser var.
+//des que la variables ne change pas alors c'est const
+//on mes tous le temps const just aux elemnt ou l'editeur met une erreur pour les const on mettras let
+//constant operateur de affectation de valeur  chaine de caracteres (aux moins une operaantes)
+
 // Votre prénom (chaîne)
-console.log(prenom);
+const name = 'Julien';
+console.log(name);
 
 // Votre âge (nombre)
-console.log(age)
+//declaration et initialisation d'une variable
+const age = 23;
+console.log(age);
+console.log("concaner 1 méthode");
+const displayNameAndAge =  name + ' a ' + age + 'ans';
+console.log(displayNameAndAge);
+console.log("concaner 2 méthode");
+console.log(`${name} a ${age} ans`);
