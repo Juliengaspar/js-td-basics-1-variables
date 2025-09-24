7ca30fc51c30c0cbd4d3d8f2af98c69eac7faa69
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-24 10:46:09 +0200

    end ex 1.3 every cas whit a different type

--- OCR-starter/chapitre1_exercice3.js
+++ OCR-starter/chapitre1_exercice3.js
@@ -14,10 +14,20 @@ avec les valeurs 4 et 5 selon qu'elles sont de type nombre ou de type chaîne :
 - que se passe-t-il si on entoure toute l'expression (VALEUR OPÉRANDE VALEUR) de guillemets ? Pourquoi ?
 */
 
+
 // Cas 1 : 4 et 5 sont toutes deux de type nombres
+console.log( "toutes deux de type nombres");
+console.log(4+5);
 
 // Cas 2 : 4 et 5 sont toutes deux de type chaîne
+console.log( "toutes deux de type chaîne");
+console.log("4"+"5");
+
 
 // Cas 3 : 4 est de type chaîne et 5 est de type nombre
+console.log( "4 est de type chaîne et 5 est de type nombre");
+console.log("4"+5);
 
 // Cas 4 : Entourer l'expression (VALEUR OPÉRANDE VALEUR) de guillemets
+console.log( "Entourer l'expression (VALEUR OPÉRANDE VALEUR) de guillemets");
+console.log("4+5");
