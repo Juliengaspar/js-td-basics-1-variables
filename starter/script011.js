2c42b59671751c72cab3f361b2e006feae722e4b
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-25 14:25:24 +0200

    end ex script011 calcule , operateur multiple & operateur de comperaison

--- starter/script011.js
+++ starter/script011.js
@@ -4,12 +4,21 @@
 
 // Opérateurs multiples
 // 1. Utilisez les opérateurs +, *, et / pour effectuer une opération mathématique complexe. Affichez le résultat.
-
+const number1 = 5
+const number2 = 10
+console.log(number1+number2);
+console.log(number1*number2);
+console.log(number1/number2);
 // Groupement
 // 2. Créez une expression mathématique qui nécessite le groupement des opérations. Utilisez des parenthèses pour clarifier l'ordre des opérations. Affichez le résultat.
-
+console.log(number1 + (number2*number2));
 // Affectations multiples
 // 3. Déclarez deux variables x et y. Utilisez des affectations multiples pour leur donner des valeurs. Par exemple, x = 5 et y = 10. Affichez les valeurs de x et y.
-
+const x = 5;
+const y = 6;
+console.log( x , y)
 // Autres opérateurs
 // 4. Utilisez un opérateur de comparaison (comme >, <, ==, etc.) pour comparer les valeurs de deux variables et affichez le résultat.
+console.log(x>y);
+console.log(x<y);
+console.log(x===y);
