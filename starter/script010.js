578314b171523db9e3c4d19919b2924a55a47a0e
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-25 14:21:35 +0200

    end ex script010 calcule age

--- starter/script010.js
+++ starter/script010.js
@@ -3,15 +3,31 @@
  */
 
 // 1. Déclarez les variables now, ageJohn, ageMark, yearJohn et yearMark, puis affectez aux trois premières les valeurs 2019, 28 et 33.
-
+let  now = 2019;
+const ageJohn = 28;
+const ageMark = 33;
+let yearJohn;
+let yearMark;
 // Opérateurs mathématiques
 
 // 2. Affectez à yearJohn sa date de naissance (en la calculant à partir de l'année courante et de son âge). Faites de même pour yearMark. Ensuite, affichez la date de naissance de John et "Mark est né en …".
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
 
+console.log(`Mark est né en ${yearJohn}.`);
+console.log(`John est né en ${yearJohn}.`);
 // 3. Affichez l'année à laquelle nous serons dans deux ans, le double de l'année et le dixième de l'année.
-
+now = now+2;
+console.log(now)
+now = 2019;
+now = now*2;
+console.log(now)
+now = 2019;
+now = now/10;
+console.log(now)
 // Opérateurs logiques
 
 // 4. … (Veuillez préciser votre question ou vos actions ici)
 
 // Opérateur typeof
+console.log(typeof now);
