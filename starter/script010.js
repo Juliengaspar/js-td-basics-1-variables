1db684df86dff5a45e388802763846e979f7f854
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-01 14:23:57 +0200

    operateur logiques

--- starter/script010.js
+++ starter/script010.js
@@ -26,7 +26,24 @@ now = 2019;
 now = now/10;
 console.log(now)
 // Opérateurs logiques
+const ageToto = 21;
+const ageTiti = 21;
 
+console.log(ageTiti>ageToto);//plus grand que
+console.log(ageTiti>=ageToto);//plus grand ou egale que
+console.log(ageTiti<ageToto);//plus petits que
+console.log(ageTiti<=ageToto);//plus petits ou egales que
+//console.log(ageTiti == ageToto);//est egale à ==> false
+console.log(ageTiti === ageToto);//est strictement ===  à valeur et le type et donc là ===> true
+console.log(ageTiti !== ageToto);//est differnt !==  à valeur et le type et donc là ===> false
+console.log(typeof ageToto);//number
+console.log(typeof ageTiti);//string
+
+console.log(!true);//false
+
+console.log((ageTiti > 18) &&(ageTiti<21));//true
+console.log((ageTiti > 18) || (ageTiti<21));//true
+console.log(1 && 1);//true
 // 4. … (Veuillez préciser votre question ou vos actions ici)
 
 // Opérateur typeof
