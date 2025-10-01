f0324582e6d58b6ec42c00f260a0c6ef4718b2eb
Juliengaspar <julien.gaspar@student.hepl.be>
2025-10-01 14:41:48 +0200

    // Affectations multiples

--- starter/script011.js
+++ starter/script011.js
@@ -16,7 +16,12 @@ console.log(number1 + (number2*number2));
 // 3. Déclarez deux variables x et y. Utilisez des affectations multiples pour leur donner des valeurs. Par exemple, x = 5 et y = 10. Affichez les valeurs de x et y.
 const x = 5;
 const y = 6;
-console.log( x , y)
+console.log( x , y);
+//let toto = 5, titi=6, tata= 9,
+//let toto, titi, tata = 5;
+const toto = 1.5;
+//en informatique les nombres reel sont fait avec un .
+
 // Autres opérateurs
 // 4. Utilisez un opérateur de comparaison (comme >, <, ==, etc.) pour comparer les valeurs de deux variables et affichez le résultat.
 console.log(x>y);
